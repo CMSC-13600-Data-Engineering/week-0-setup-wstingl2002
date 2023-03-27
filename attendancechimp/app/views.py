0fe0b1c8f5c2c50695a27416be116c85045afdfe
wstingl2002 <wiilliamstingl@wiilliams-MacBook-Pro-2.local>
2023-03-26 19:26:27 -0500

    my submission

--- attendancechimp/app/views.py
+++ attendancechimp/app/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'app/index.html', {})
+    bio = "My name is William Stingl a UChicago student in data engineering."
+    dictionary = {'bio': bio}
+    return render(request, 'app/index.html', dictionary)
